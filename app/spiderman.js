277b6ce43a05e2eeecc5d16abad9a869c9d1f323
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-24 17:02:10 -0500

    Paso 9: Agregando el constructor a la clase Spiderman

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,6 +1,12 @@
 // Creando la clase Spiderman
 class Spiderman{
-
+    constructor(name, age, actor, movies, studio) {
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.movies = movies
+        this.studio = studio
+    }
 }
 
 // Exportando la clase
